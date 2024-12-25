4047b5d057b9993ad594c9aceab8262f6aa1f6e2
SidSigmaX <sidsigma3@gmail.com>
2024-12-26 05:15:01 +0530

    changes

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 
 const corsOptions = {
+    methods: ['GET' ,'POST', 'OPTIONS'],
     origin: ['http://localhost:3000', 'https:www.stockangel.online'],
     credentials: true, // Allow cookies and credentials
   };
