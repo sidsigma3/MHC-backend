1659a2b1ff94de7269651bf31a37bf5525345e98
SidSigmaX <sidsigma3@gmail.com>
2024-12-27 18:18:02 +0530

    changes

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const PORT = process.env.PORT || 5000;
 
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://mhc-frontend.vercel.app'], // Allow these origins
-  methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'], // Allowed HTTP methods
+  origin: ['http://localhost:3000', 'https://www.stockangel.online'], 
+  methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'], 
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
   };
@@ -33,10 +33,10 @@ app.use(ErorrHandler);
 
 app.use(
     session({
-      secret: process.env.SESSION_SECRET, // Store in .env file
+      secret: process.env.SESSION_SECRET,
       resave: false,
       saveUninitialized: false,
-      cookie: { secure: false }, // Use `true` if HTTPS is enabled
+      cookie: { secure: false }, 
     })
   );
 
