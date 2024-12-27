89f0670f56fd19ac1671f2897c3c79f7b665c4c2
SidSigmaX <sidsigma3@gmail.com>
2024-12-27 10:49:21 +0530

    changes

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
   origin: ['http://localhost:3000', 'https://mhc-frontend.vercel.app'], // Allow these origins
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
   };
 app.use(cors(corsOptions));
