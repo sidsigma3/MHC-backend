b954b642cc84a6fe3c6b2746e56d93e8d08481d5
SidSigmaX <sidsigma3@gmail.com>
2025-01-02 17:09:01 +0530

    changes

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -5,14 +5,14 @@ const fs = require('fs');
 const path = require('path');
 
 
-const uploadsDir = path.join(__dirname, 'uploads');
+const uploadsDir = path.join(__dirname, 'Upload');
 if (!fs.existsSync(uploadsDir)) {
   fs.mkdirSync(uploadsDir, { recursive: true });
 }
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads'); // Directory where files will be stored
+        cb(null, 'Upload');
     },
     filename: (req, file, cb) => {
         cb(null, `${req.body.userId}-${Date.now()}-${file.originalname}`); // Unique file name
