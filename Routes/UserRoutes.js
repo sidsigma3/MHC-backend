8c11fb3a48b873ce2400c2bc86704b2fe94b1a5d
SidSigmaX <sidsigma3@gmail.com>
2025-01-02 16:55:49 +0530

    changes

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, './../upload'); 
+      cb(null, './../Upload'); 
     },
     filename: (req, file, cb) => {
       cb(null, `${req.body.userId}-${Date.now()}-${file.originalname}`); // Unique file name
