e0bf8cc6fe22d6959ee6afc4a156de9743473887
SidSigmaX <sidsigma3@gmail.com>
2025-01-03 18:20:36 +0530

    changes

--- Config/db.js
+++ Config/db.js
@@ -2,15 +2,17 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
   dialect: 'mysql',
   dialectModule: require('mysql2'),
+  dialectOptions: {
+    socketPath: process.env.CC_MYSQL_PROXYSQL_SOCKET_PATH,
+  },
   pool: {
-    max: 50, 
-    min: 0, 
-    acquire: 30000, 
-    idle: 10000 
-}
+    max: 50,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 });
 
 (async () => {
