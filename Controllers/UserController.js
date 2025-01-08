4c34c678df799e31426d67c81ee86d045cce4ebe
SidSigmaX <sidsigma3@gmail.com>
2025-01-08 14:54:31 +0530

    changes

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -149,11 +149,10 @@ const createUser = async (req, res) => {
 
       res.cookie("token", jwtToken, {
         httpOnly: true,        // Prevent JavaScript access
-        secure: false, // Use HTTPS in production
+        secure: true, // Use HTTPS in production
         sameSite: "none",  
         maxAge: 1 * 60 * 60 * 1000, // 1 hour
-        domain: 'localhost', // Optional: For subdomains
-        path: '/' // Optional: Default, accessible to all paths
+        path: '/' 
       });
   
       
